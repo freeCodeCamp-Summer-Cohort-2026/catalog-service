b5349002c026017a048ae9cab378e4c049c15f85
shy_away <149800599+shy-away@users.noreply.github.com>
2026-08-12 15:56:13 -0500

    fix: restrict logging to `/api/products` routes implemented using a FilterRegistrationBean in new file LoggerRegistration.java

---
+++ src/main/java/com/nhcarrigan/catalogservice/config/LoggerRegistration.java
@@ -0,0 +1,19 @@
+package com.nhcarrigan.catalogservice.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.nhcarrigan.catalogservice.filter.LoggingFilter;
+
+@Configuration
+public class LoggerRegistration {
+
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> logFilter() {
+        FilterRegistrationBean<LoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoggingFilter());
+        registrationBean.addUrlPatterns("/api/products");
+        return registrationBean;
+    }
+}
--- src/main/java/com/nhcarrigan/catalogservice/filter/LoggingFilter.java
+++ src/main/java/com/nhcarrigan/catalogservice/filter/LoggingFilter.java
@@ -8,9 +8,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 public class LoggingFilter implements Filter {
 
     Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
