a23d48436e46a9407a76588e1c5db7ee104bf38f
River KArnas <river.karnas@gmail.com>
2026-08-13 15:15:49 -0300

    Fix imports, method visibility, and API path in health check after mantainer review #4

--- src/test/java/com/nhcarrigan/catalogservice/controller/HealthControllerTest.java
+++ src/test/java/com/nhcarrigan/catalogservice/controller/HealthControllerTest.java
@@ -1,10 +1,17 @@
 package com.nhcarrigan.catalogservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
 
 import java.util.Map;
-import java.beans.Transient;
 import java.util.HashMap;
 
 @SpringBootTest
@@ -19,6 +26,7 @@ public class HealthControllerTest{
     @Test
     void healthCheckReturnServiceStatus() throws Exception{
         mockMvc.perform(get("/health"))
-        .andExpect(status().isOk().jsonPath("$.service", is("200 OK")));
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.service", is("status: UP")));
     }
 }
