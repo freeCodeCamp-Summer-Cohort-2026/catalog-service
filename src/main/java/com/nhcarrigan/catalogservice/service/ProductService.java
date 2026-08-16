6794ca67cf7011b7eb1c4fe3c9b9312529603a2f
Prakash Sankhla <prakashsankhla0601@gmail.com>
2026-08-17 01:13:20 +0530

    import Collectors for stream operations in ProductService

--- src/main/java/com/nhcarrigan/catalogservice/service/ProductService.java
+++ src/main/java/com/nhcarrigan/catalogservice/service/ProductService.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
